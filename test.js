3dcb7691104aa7f35dc035fb7e73a7e5ff0dd4b6
igoradamenko <mail@igoradamenko.com>
2021-03-24 20:17:23 +0200

    Add tests

---
+++ test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+const package = require('.');
+
+describe('Main suite', () => {
+  it('should equal 0', () => {
+    // TODO: fix it when we implement the package
+    assert.deepStrictEqual(package, 0);
+  })
+});
